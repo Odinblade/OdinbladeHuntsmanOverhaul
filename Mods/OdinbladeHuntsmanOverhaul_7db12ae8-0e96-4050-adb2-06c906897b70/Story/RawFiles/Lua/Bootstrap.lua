7eba8564bfc06b0f3eb74e110aaaa1c483d3a71e
TheOdinblade <50718347+TheOdinblade@users.noreply.github.com>
2020-02-24 20:22:09 +0000

    Full mod release!

--- Mods/OdinbladeHuntsmanOverhaul_7db12ae8-0e96-4050-adb2-06c906897b70/Story/RawFiles/Lua/Bootstrap.lua
+++ Mods/OdinbladeHuntsmanOverhaul_7db12ae8-0e96-4050-adb2-06c906897b70/Story/RawFiles/Lua/Bootstrap.lua
@@ -10,25 +10,25 @@ local function OverrideStats()
 
     for statname,overrides in pairs(OdinHUN_StatOverrides) do
         for property,value in pairs(overrides) do
-            if debug_print then Ext.Print("OdinHUN:Bootstrap.lua] Overriding stat: " .. statname .. " (".. property ..") = \"".. value .."\"") end
+            if debug_print then Ext.Print("[OdinHUN:Bootstrap.lua] Overriding stat: " .. statname .. " (".. property ..") = \"".. value .."\"") end
             if property == "SkillProperties" then 
                 Ext.StatAddCustomDescription(statname, property, value) 
-                if debug_print then Ext.Print("OdinHUN:Bootstrap.lua] Custom description set: " .. statname .. " (".. property ..") = \"".. value .."\"") end
+                if debug_print then Ext.Print("[OdinHUN:Bootstrap.lua] Custom description set: " .. statname .. " (".. property ..") = \"".. value .."\"") end
             else 
                 Ext.StatSetAttribute(statname, property, value)
-                if debug_print then Ext.Print("OdinHUN:Bootstrap.lua] Stat attribute overwritten: " .. statname .. " (".. property ..") = \"".. value .."\"") end
+                if debug_print then Ext.Print("[OdinHUN:Bootstrap.lua] Stat attribute overwritten: " .. statname .. " (".. property ..") = \"".. value .."\"") end
             end
             total_changes = total_changes + 1
         end
         total_stats = total_stats + 1
     end
 
-    Ext.Print("OdinHUN:Bootstrap.lua] Changed ("..tostring(total_changes)..") properties in ("..tostring(total_stats)..") stats.")
+    Ext.Print("[OdinHUN:Bootstrap.lua] Changed ("..tostring(total_changes)..") properties in ("..tostring(total_stats)..") stats.")
 end
 
 local ModuleLoading = function ()
     Ext.Print("[OdinHUN:Bootstrap.lua] Module is loading.")
-    OverrideStats()
+    -- OverrideStats()
 end
 
 Ext.Require("7db12ae8-0e96-4050-adb2-06c906897b70", "OdinHUN_StatOverrides.lua")
