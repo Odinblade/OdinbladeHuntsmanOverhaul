4296f4c7818059eec32c455f537770a121e9df31
TheOdinblade <50718347+TheOdinblade@users.noreply.github.com>
2020-03-23 14:48:23 +0000

    v1.0.4.1

--- Mods/OdinbladeHuntsmanOverhaul_7db12ae8-0e96-4050-adb2-06c906897b70/Story/RawFiles/Lua/OdinHUN_StatOverrides.lua
+++ Mods/OdinbladeHuntsmanOverhaul_7db12ae8-0e96-4050-adb2-06c906897b70/Story/RawFiles/Lua/OdinHUN_StatOverrides.lua
@@ -1,14 +1,47 @@
 OdinHUN_StatOverrides = {
     Target_OdinHUN_Summon_AnimalCompanion = {
-        SkillProperties = "The stats of the summons will depend on caster level and Summoning ability."
+        SkillCustomDescription = "The stats of the summons will depend on caster level and Summoning ability."
     },
     Shout_OdinHUN_NaturalBond = {
-        SkillProperties = "Toggle to apply Natural Bond to you and your Animal Companion."
+        SkillCustomDescription = "Toggle to apply Natural Bond to you and your Animal Companion."
     },
     Shout_OdinHUN_NowhereToHide = {
-        SkillProperties = "Automatically applies Vulnerable to your farthest target."
+        SkillCustomDescription = "Automatically applies Vulnerable to your farthest target."
     },
     Shout_OdinHUN_Camouflage = {
-        SkillProperties = "Set Invisible for 1 turn(s)."
+        SkillCustomDescription = "Set Invisible for 1 turn(s)."
     },
+    Projectile_Grenade_ChemicalWarfare = {
+        SkillCustomDescription = "Basic grenade which is automatically refunded by Savvy Hunter."
+    },
+    Projectile_Grenade_Flashbang = {
+        SkillCustomDescription = "Basic grenade which is automatically refunded by Savvy Hunter."
+    },
+    Projectile_Grenade_Ice = {
+        SkillCustomDescription = "Basic grenade which is automatically refunded by Savvy Hunter."
+    },
+    Projectile_Grenade_Molotov = {
+        SkillCustomDescription = "Basic grenade which is automatically refunded by Savvy Hunter."
+    },
+    Projectile_Grenade_MustardGas = {
+        SkillCustomDescription = "Basic grenade which is automatically refunded by Savvy Hunter."
+    },
+    Projectile_Grenade_Nailbomb = {
+        SkillCustomDescription = "Basic grenade which is automatically refunded by Savvy Hunter."
+    },
+    Projectile_Grenade_OilFlask = {
+        SkillCustomDescription = "Basic grenade which is automatically refunded by Savvy Hunter."
+    },
+    Projectile_Grenade_PoisonFlask = {
+        SkillCustomDescription = "Basic grenade which is automatically refunded by Savvy Hunter."
+    },
+    Projectile_Grenade_SmokeBomb = {
+        SkillCustomDescription = "Basic grenade which is automatically refunded by Savvy Hunter."
+    },
+    Projectile_Grenade_Taser = {
+        SkillCustomDescription = "Basic grenade which is automatically refunded by Savvy Hunter."
+    },
+    Projectile_Grenade_WaterBalloon = {
+        SkillCustomDescription = "Basic grenade which is automatically refunded by Savvy Hunter."
+    }
 }
