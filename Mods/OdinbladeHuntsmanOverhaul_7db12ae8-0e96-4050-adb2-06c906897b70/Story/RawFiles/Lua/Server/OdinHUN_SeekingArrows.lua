97a9d6c179f9b2fd1ce7103751843673b2eaf4a1
Odinblade <50718347+Odinblade@users.noreply.github.com>
2025-12-28 23:27:49 +0000

    Add OnBeforeSortAiSections for Seeking Arrows skill

--- Mods/OdinbladeHuntsmanOverhaul_7db12ae8-0e96-4050-adb2-06c906897b70/Story/RawFiles/Lua/Server/OdinHUN_SeekingArrows.lua
+++ Mods/OdinbladeHuntsmanOverhaul_7db12ae8-0e96-4050-adb2-06c906897b70/Story/RawFiles/Lua/Server/OdinHUN_SeekingArrows.lua
@@ -42,3 +42,61 @@ Ext.Events.StatusHitEnter:Subscribe(function (e)
         end
     end
 end)
+
+Ext.Events.OnBeforeSortAiActions:Subscribe(function (e)
+    local seekingArrowsAction = nil
+    local seekingArrowsActionIndex = nil
+    local boosted = false
+
+    -- Find the SeekingArrows action
+    for i, action in ipairs(e.Request.AiActions) do
+        if action.SkillId then
+            if GetSkillEntryName(action.SkillId) == "Shout_OdinHUN_Enemy_SeekingArrows" then
+                seekingArrowsAction = action
+                seekingArrowsActionIndex = i
+                break
+            end
+        end
+    end
+
+    -- If found, check other actions for ranged weapon skills or StandardAttack
+    if seekingArrowsAction then
+        local char = Ext.ServerEntity.GetCharacter(e.CharacterHandle)
+        if char.Stats.CurrentAP <= 3 then
+            seekingArrowsAction.FinalScore = 0
+            seekingArrowsAction.ActionFinalScore = 0
+            return
+        end
+
+        for i, action in ipairs(e.Request.AiActions) do
+            if i ~= seekingArrowsActionIndex then
+                -- If action has a SkillId, check its requirement
+                if action.SkillId and action.SkillId ~= "" then
+                    local entryName = GetSkillEntryName(action.SkillId)
+                    local stat = Ext.Stats.Get(entryName)
+                    if stat and stat.Requirement == "RangedWeapon" then
+                        seekingArrowsAction.FinalScore = 999
+                        seekingArrowsAction.ActionFinalScore = 999
+                        boosted = true
+                        break
+                    end
+                end
+
+                -- If it's a standard attack (may have empty SkillId), boost the seeking arrows score
+                if action.ActionType == "StandardAttack" then
+                    seekingArrowsAction.FinalScore = 999
+                    seekingArrowsAction.ActionFinalScore = 999
+                    boosted = true
+                    break
+                end
+            end
+        end
+
+        -- If not boosted, set FinalScore to 0
+        if not boosted then
+            seekingArrowsAction.FinalScore = 0
+            seekingArrowsAction.ActionFinalScore = 0
+        end
+    end
+end)
+
